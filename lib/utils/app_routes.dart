dd2b3eb7cac5d649b812248f680108b9cad50775
os <kkeval304@gmail.com>
2024-09-04 14:15:14 +0530

    new

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hospital_management_system/screen/home/view/home_screen.dart';
+
+Map<String,WidgetBuilder> app_routes={
+  '/': (context) => const HomeScreen(),
+};
