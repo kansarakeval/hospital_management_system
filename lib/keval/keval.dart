ead0e2d714c106f2c777ac023693eae99ce91636
os <kkeval304@gmail.com>
2024-09-15 15:20:13 +0530

    tasting commit

---
+++ lib/keval/keval.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Keval extends StatefulWidget {
+  const Keval({super.key});
+
+  @override
+  State<Keval> createState() => _KevalState();
+}
+
+class _KevalState extends State<Keval> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
